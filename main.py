5123a3643e5663bbec0b20f0a9bc43d63425b96d
Jaineel135 <jaineeldharmawat@gmail.com>
2025-01-19 03:05:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,3 @@ result = run_flow_from_json(flow="Basic Prompting (1).json",
                             session_id="", # provide a session id if you want to use session state
                             fallback_to_env_vars=True, # False by default
                             tweaks=TWEAKS)
-
